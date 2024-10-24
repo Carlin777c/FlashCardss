4a5d43fdc3d1476642f072fb27ce39fdf43016c4
Carlin777c <carlos.machetti@escola.pr.gov.br>
2024-10-24 10:10:00 -0300

    .

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('.flashcard').forEach(card => {
-    card.addEventListener('.click', () => {
-        card.classList.toggle('flip')
-    })
-})
+    card.addEventListener('click', () => {
+        card.classList.toggle('flip');
+    });
+});
